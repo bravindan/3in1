9fc7c209d9cd10d74753247c85b829d190ad69cb
bravindan <53007654+bravindan@users.noreply.github.com>
2023-02-02 17:21:07 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import { FaHome } from 'react-icons/fa'
 
 export default function Navbar() {
   return (
     <main className='sticky top-0 z-10'>
-        <section className="mx-auto h-16 bg-sky-500 text-white shadow-lg flex  items-center justify-between">
-        <div>
-        <img src={logo} className="animate-spin ml-4" alt="logo" width={40} />
+        <section className="mx-auto h-16 bg-sky-500 text-white shadow-lg flex items-center justify-between md:justify-between ">
+        <div className=''>
+        <Link to='/'><FaHome className='text-2xl ml-4'/></Link>
+
         </div>
         <div>
-            <ul className='flex m-3 py-3 justify-center items-center gap-x-10'>
+            <ul className='flex md:m-3 py-3 justify-center items-center gap-x-4 md:gap-x-10'>
 
-            <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
-            <Link to='/'>Home</Link>
-            </li>
             <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
             <Link to='/movies'>Movies</Link>
             </li>
             <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
-            <Link to='/news'>News</Link>
-            </li>
-            <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
             <Link to='/weather'>Weather</Link>
             </li>
+            <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
+            <Link to='/news'>News</Link>
+            </li>
+           
             </ul>
             </div>
         </section>
