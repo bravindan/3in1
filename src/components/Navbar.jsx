d1d83f72327f3b127e8de416c7c06991b3c9681c
bravindan <53007654+bravindan@users.noreply.github.com>
2023-02-02 17:49:02 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import { FaHome } from 'react-icons/fa'
 export default function Navbar() {
   return (
     <main className='sticky top-0 z-10'>
-        <section className="mx-auto h-16 bg-sky-500 text-white shadow-lg flex items-center justify-between md:justify-between ">
+        <section className="mx-auto h-16 bg-sky-500 text-white shadow-lg flex items-center justify-even md:justify-between ">
         <div className=''>
         <Link to='/'><FaHome className='text-2xl ml-4'/></Link>
 
         </div>
         <div>
-            <ul className='flex md:m-3 py-3 justify-center items-center gap-x-4 md:gap-x-10'>
+            <ul className='flex ml-4 md:m-3 py-3 justify-center items-center gap-x-4 md:gap-x-10'>
 
             <li className='hover:bg-sky-700 rounded-md px-4 duration-300 '>
             <Link to='/movies'>Movies</Link>
